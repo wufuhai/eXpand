e64aac6c640174497df1d9ea73708993f30aa44e
wufuhai <wufuhai@qne.com.my>
2016-08-03 13:18:00 +0800

    Customize ModelDifference module to meet the requirement of QNE Business Solutions

--- Xpand/Xpand.ExpressApp.Modules/ModelDifference/ModelDifferenceModule.cs
+++ Xpand/Xpand.ExpressApp.Modules/ModelDifference/ModelDifferenceModule.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using DevExpress.ExpressApp;
 using DevExpress.ExpressApp.DC;
-using DevExpress.ExpressApp.Model;
 using DevExpress.ExpressApp.Model.Core;
 using DevExpress.ExpressApp.Security;
 using DevExpress.Utils;
@@ -11,55 +10,71 @@ using Xpand.ExpressApp.ModelDifference.DataStore.BaseObjects;
 using Xpand.ExpressApp.ModelDifference.NodeUpdaters;
 using Xpand.ExpressApp.ModelDifference.Security.Improved;
 using Xpand.Persistent.Base.General;
+using DevExpress.Persistent.AuditTrail;
 
-
-namespace Xpand.ExpressApp.ModelDifference {
-    public interface IModelOptionsUserModelDifferenceObjectSubjectTemplate{
-        [Category(ModelDifferenceModule.ModelDifferenceCategory)]
-        [DefaultValue("Autocreated at {0} For {1}")]
-        string UserModelDifferenceObjectSubjectTemplate { get; set; }
-    }
+namespace Xpand.ExpressApp.ModelDifference
+{
     [ToolboxItem(true)]
     [ToolboxTabName(XpandAssemblyInfo.TabWinWebModules)]
-    public sealed class ModelDifferenceModule : XpandModuleBase, ISequenceGeneratorUser{
+    public sealed class ModelDifferenceModule : XpandModuleBase, ISequenceGeneratorUser
+    {
         public const string ModelDifferenceCategory = "eXpand.ModelDifference";
-        public ModelDifferenceModule() {
+        public ModelDifferenceModule()
+        {
             RequiredModuleTypes.Add(typeof(DevExpress.ExpressApp.CloneObject.CloneObjectModule));
             RequiredModuleTypes.Add(typeof(ExpressApp.Security.XpandSecurityModule));
         }
 
-        public override void ExtendModelInterfaces(ModelInterfaceExtenders extenders){
-            base.ExtendModelInterfaces(extenders);
-            extenders.Add<IModelOptions, IModelOptionsUserModelDifferenceObjectSubjectTemplate>();
-        }
-
-        public override void CustomizeTypesInfo(ITypesInfo typesInfo) {
+        public override void CustomizeTypesInfo(ITypesInfo typesInfo)
+        {
             base.CustomizeTypesInfo(typesInfo);
-            if (!RuntimeMode) {
+            if (!RuntimeMode)
+            {
                 CreateDesignTimeCollection(typesInfo, typeof(UserModelDifferenceObject), "Users");
                 CreateDesignTimeCollection(typesInfo, typeof(RoleModelDifferenceObject), "Roles");
-            } else if (Application.CanBuildSecurityObjects()) {
+            }
+            else if (Application.CanBuildSecurityObjects())
+            {
                 BuildSecuritySystemObjects();
             }
         }
 
-        public override void Setup(XafApplication application) {
+        public override void Setup(XafApplication application)
+        {
             base.Setup(application);
-            if (application != null && !DesignMode) {
+
+            AuditTrailService.Instance.CustomizeAuditTrailSettings += Instance_CustomizeAuditTrailSettings;
+            if (application != null && !DesignMode)
+            {
                 application.SettingUp += ApplicationOnSettingUp;
             }
-            AddToAdditionalExportedTypes(typeof(ModelDifferenceObject).Namespace, GetType().Assembly);
+            if (RuntimeMode)
+            {
+                AddToAdditionalExportedTypes(typeof(ModelDifferenceObject).Namespace, GetType().Assembly);
+            }
         }
 
-        void ApplicationOnSettingUp(object sender, EventArgs eventArgs) {
+        private void Instance_CustomizeAuditTrailSettings(object sender, CustomizeAuditTrailSettingsEventArgs e)
+        {
+            e.AuditTrailSettings.RemoveType(typeof(PersistentApplication));
+            e.AuditTrailSettings.RemoveType(typeof(ModelDifferenceObject));
+            e.AuditTrailSettings.RemoveType(typeof(RoleModelDifferenceObject));
+            e.AuditTrailSettings.RemoveType(typeof(UserModelDifferenceObject));
+            e.AuditTrailSettings.RemoveType(typeof(AspectObject));
+        }
+
+        void ApplicationOnSettingUp(object sender, EventArgs eventArgs)
+        {
             BuildSecuritySystemObjects();
             var securityStrategy = ((XafApplication)sender).Security as SecurityStrategy;
-            if (securityStrategy != null) {
+            if (securityStrategy != null)
+            {
                 (securityStrategy).CustomizeRequestProcessors += OnCustomizeRequestProcessors;
             }
         }
 
-        void BuildSecuritySystemObjects() {
+        void BuildSecuritySystemObjects()
+        {
             var dynamicSecuritySystemObjects = new DynamicSecuritySystemObjects(Application);
             var xpMemberInfos = dynamicSecuritySystemObjects.BuildUser(typeof(UserModelDifferenceObject), "UserUsers_UserModelDifferenceObjectUserModelDifferenceObjects", "UserModelDifferenceObjects", "Users");
             dynamicSecuritySystemObjects.HideInDetailView(xpMemberInfos, "UserModelDifferenceObjects");
@@ -67,12 +82,14 @@ namespace Xpand.ExpressApp.ModelDifference {
             dynamicSecuritySystemObjects.HideInDetailView(xpMemberInfos, "RoleModelDifferenceObjects");
         }
 
-        void OnCustomizeRequestProcessors(object sender, CustomizeRequestProcessorsEventArgs customizeRequestProcessorsEventArgs) {
+        void OnCustomizeRequestProcessors(object sender, CustomizeRequestProcessorsEventArgs customizeRequestProcessorsEventArgs)
+        {
             var modelCombineRequestProcessor = new ModelCombineRequestProcessor(customizeRequestProcessorsEventArgs.Permissions);
             var keyValuePair = new KeyValuePair<Type, IPermissionRequestProcessor>(typeof(ModelCombinePermissionRequest), modelCombineRequestProcessor);
             customizeRequestProcessorsEventArgs.Processors.Add(keyValuePair);
         }
-        public override void AddGeneratorUpdaters(ModelNodesGeneratorUpdaters updaters) {
+        public override void AddGeneratorUpdaters(ModelNodesGeneratorUpdaters updaters)
+        {
             base.AddGeneratorUpdaters(updaters);
             updaters.Add(new BOModelNodesUpdater());
             updaters.Add(new BOModelMemberNodesUpdater());
